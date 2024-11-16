35bccc7b705df800952371a53b970d7da1784951
mmrho <mmohammadrezaho@gmail.com>
2024-11-16 14:22:28 +0330

    wp-enqueue

--- functions.php
+++ functions.php
@@ -1,2 +1,10 @@
 <?php
+define('THEME_ASSETS', get_template_directory_uri() . '/assets/');
+define('THEME_NODE', get_template_directory_uri() . '/node_modules/');
+const THEME_JS = THEME_ASSETS . 'js/',
+THEME_CSS = THEME_ASSETS . 'css/',
+THEME_VERSION = '1.0';
+define("THEME_IMG", get_template_directory_uri() . '/images/');
+define('THEME_TEMPLATE', get_template_directory() . '/inc/template/');
+define('THEME_OPTIONS', get_option('themeOptions'));
 require_once "vendor/autoload.php";
---
+++ inc/functions/wp-enqueue.php
@@ -0,0 +1,18 @@
+<?php
+function wbs_enqueue_scripts() {
+    wp_enqueue_style( 'bootstrap-grid', THEME_CSS . 'bootstrap-grid.rtl.min.css' );
+    wp_enqueue_style( 'bootstrap-reboot', THEME_CSS .  'bootstrap-reboot.rtl.min.css' );
+    wp_enqueue_style( 'owl-carousel', THEME_ASSETS .  'plugins/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css' );
+    wp_enqueue_style( 'owl-theme-default', THEME_ASSETS .  'plugins/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css' );
+    wp_enqueue_style( 'icons', THEME_ASSETS .  'fonts/icons/css/icons.css' );
+    wp_enqueue_style( 'style', get_stylesheet_uri(),[], THEME_VERSION );
+
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', includes_url() . 'js/jquery/jquery.js', array(), '3.7.1', true);
+    wp_enqueue_script('jquery');
+
+    wp_enqueue_script('owl-carousel',THEME_ASSETS .  'plugins/OwlCarousel2-2.3.4/dist/owl.carousel.min.js',['jquery'],THEME_VERSION, true);
+    wp_enqueue_script('script',THEME_JS .  'script.js',['jquery'],THEME_VERSION, true);
+
+}
+add_action( 'wp_enqueue_scripts', 'wbs_enqueue_scripts');
